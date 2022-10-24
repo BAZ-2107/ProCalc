6f0c109bd29527f5f4fea2895b8eb65e0b58010a
VAZ-2107 <94621971+BAZ-2107@users.noreply.github.com>
2022-10-25 00:09:56 +0300

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-import os
-for file in ['check_string.py', 'DecodeExpression.py', 'decoder.py', 'exceptions.py', 'for_excercise.py', 'main.py', 'objects.py', 'output.py']:
-#array = os.chdir(os.path.join("C:\Users\PC\Documents\Pro_calc"))
-    a = open(file).read()
-    with open(file, "w") as x:
-        print(a.replace("\t", "    "), file=x)
