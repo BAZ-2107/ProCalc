5790923294d135838c246c00323c6b14d6d366f4
VAZ-2107 <94621971+BAZ-2107@users.noreply.github.com>
2022-12-29 17:07:24 +0300

    Add files via upload

--- for_tg/keyboards.py
+++ for_tg/keyboards.py
@@ -4,7 +4,7 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 class Keyboards:
-    text = InlineKeyboardButton(' ', callback_data='text')
+    text = InlineKeyboardButton('✍', callback_data='text')
     one = InlineKeyboardButton('1', callback_data='1')
     two = InlineKeyboardButton('2', callback_data='2')
     three = InlineKeyboardButton('3', callback_data='3')
@@ -35,31 +35,35 @@ class Keyboards:
     left_bracket = InlineKeyboardButton('(', callback_data='(')
     module_bracket = InlineKeyboardButton('|', callback_data='|')
     
-    del_last = InlineKeyboardButton('CE', callback_data='CE')
-    del_all = InlineKeyboardButton('C', callback_data='C')
+    del_last = InlineKeyboardButton('⌫', callback_data='⌫')
+    del_all = InlineKeyboardButton('❌', callback_data='❌')
     
-    running = InlineKeyboardButton('Решить', callback_data='running')
+    running = InlineKeyboardButton('✅', callback_data='✅')
     
     back = InlineKeyboardButton('Назад', callback_data='back')
     
-    letters = InlineKeyboardButton('Буквы', callback_data='letters')
+    letters = InlineKeyboardButton('f(x)', callback_data='funcs')
+
+    pi = InlineKeyboardButton('π', callback_data='π')
+
+    exp = InlineKeyboardButton('e', callback_data='e')
     
     
     button_list = [[text],
                    [left_bracket, module_bracket, right_bracket, del_last, del_all],
                    [one, two, three, add, div], [four, five, six, sub, poww],
                    [seven, eight, nine, mul, sqrt],
-                    #[more, more_or_equal, equal, less_or_equal, less],
-                   [zero, comm, letters, running]]
+                    [zero, comm, pi, exp, letters],
+                   [running]]
     
     
-    symbols, array = open("txt/symbols.txt", encoding="utf-8").read()[25:], []
-    for i in range(8):
-        array.append([InlineKeyboardButton(letter, callback_data=letter) for letter in symbols[:8]])
-        symbols = symbols[8:]
+    symbols, array = open("txt/functions.txt", encoding="utf-8").read().split(), []
+    for i, row in enumerate(symbols):
+        if i % 3 == 0:
+            array.append([])     
+        array[-1] += [InlineKeyboardButton(row, callback_data=row)]
     
-    button_list_2 = [[text],
-                     *array]
+    button_list_2 = [[text], *array]
     
     button_list_2[-1] += [del_last, del_all, back]    
 
